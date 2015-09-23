8a74528167ebc7021067dbc19bc23ae7c58185f1
Fisher Liang <swimnose@gmail.com>
2015-09-23 16:25:03 +0800

    Fix the issue Facebook return nulled email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
@@ -9,11 +7,19 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.name = auth.info.name   # assuming the user model has a name
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.name = auth.info.name
       end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
   end
 end
